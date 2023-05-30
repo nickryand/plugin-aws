07529faf4c7c21b1d5ed904a1e7d43c25c80f353
Nick Downs <nickryand@gmail.com>
2023-05-30 10:27:52 -0700

    fixed sed regex to find filters properly

--- functions/aws.fish
+++ functions/aws.fish
@@ -4,14 +4,15 @@ function aws -a cmd -d 'Universal CLI for AWS'
       if set -q argv[2]
         set -gx AWS_PROFILE "$argv[2]"
       else if set -q FILTER
-        aws profiles | command env $FILTER | read -gx AWS_PROFILE
+        # aws profiles | command env $FILTER | read -gx AWS_PROFILE
+        aws profiles | command sed -n -e 's/^\[profile \(.*\)\]/\1/p' | read -gx AWS_PROFILE
         echo $AWS_PROFILE
       else
         echo $AWS_PROFILE
       end
 
     case profiles
-      command sed -n -e 's/^\[profile \(.*\)\]/\1/p' "$HOME/.aws/config"
+      command cat ~/.aws/{config,credentials} | string replace -r -f "^\[(profile[[:space:]]*)*(.*)\]" '$2' | sort -u
 
     case '*'
       command aws $argv
